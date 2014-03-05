a31baca5cef5732626ec4dd2a74531b52d338358
hikaru.tooyama <hikaru.tooyama@nanapi.co.jp>
2014-03-05 09:06:05 +0900

    add totalspace2

---
+++ Casks/totalspaces2.rb
@@ -0,0 +1,7 @@
+class Totalspaces < Cask
+  url 'http://downloads.binaryage.com/TotalSpaces2-2.0.17.zip'
+  homepage 'http://totalspaces.binaryage.com/'
+  version '2.0.17'
+  sha256 '37b044a2aa61527a4973345d25ac86d74a66c9ddd3a884ed80293c3d7946afb3'
+  install 'TotalSpaces2.pkg'
+end
